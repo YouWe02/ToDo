11cc599e0146bfa97625b7dd804d7b55894118fa
Aaron Beer <aaron.beer@ict-campus.net>
2018-05-31 15:06:15 +0200

    monthview finished

--- app/src/main/java/net/ictcampus/weberyo/todo/ImportDates.java
+++ app/src/main/java/net/ictcampus/weberyo/todo/ImportDates.java
@@ -24,13 +24,18 @@ public class ImportDates {
         String actualDateFormatted = dateFormatter.format(actualDate);
         try{
             Date date = dateFormatter.parse(actualDateFormatted);
-            maximalDate = new Date(date.getYear() + 1, date.getMonth(), date.getDate());
-            minimalDate = new Date(date.getYear() - 1, date.getMonth(), date.getDate());
+            if(date.getMonth() != 11) {
+                maximalDate = new Date(date.getYear() + 1, date.getMonth() + 1, 1);
+                minimalDate = new Date(date.getYear() - 1, date.getMonth(), 1);
+            }
+            else{
+                maximalDate = new Date(date.getYear() + 2, 0, 1);
+                minimalDate = new Date(date.getYear() - 1, date.getMonth(), 1);
+            }
         }
         catch (java.text.ParseException e){
             e.printStackTrace();
         }
-        Log.i("TAAAAAAG", dateFormatter.format(minimalDate) + "" + dateFormatter.format(maximalDate));
         return(getDatesBetween(minimalDate, maximalDate));
     }
 
--- app/src/main/java/net/ictcampus/weberyo/todo/wocheActivity.java
+++ app/src/main/java/net/ictcampus/weberyo/todo/wocheActivity.java
@@ -1,29 +1,83 @@
 package net.ictcampus.weberyo.todo;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class wocheActivity extends AppCompatActivity {
-
+    private List<Button> allWeekButtons = new ArrayList<Button>();
+    private int resetMonth;
+    private int resetYear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_woche);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        resetMonth = intent.getIntExtra("Month", 0);
+        resetYear = intent.getIntExtra("Year", 2);
+        allWeekButtons.add((Button) findViewById(R.id.kalenderwoche1));
+        allWeekButtons.add((Button) findViewById(R.id.kalenderwoche2));
+        allWeekButtons.add((Button) findViewById(R.id.kalenderwoche3));
+        allWeekButtons.add((Button) findViewById(R.id.kalenderwoche4));
+        allWeekButtons.add((Button) findViewById(R.id.kalenderwoche5));
+        allWeekButtons.add((Button) findViewById(R.id.kalenderwoche6));
+        allWeekButtons.add((Button) findViewById(R.id.kalenderwoche7));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        for(Button a:allWeekButtons){
+            a.setOnTouchListener(new OnSwipeTouchListener(wocheActivity.this) {
+                public void onSwipeTop() {
+
+                }
+
+                public void onSwipeRight() {
+
+                }
+
+                public void onSwipeLeft() {
+
+                }
+
+                public void onSwipeBottom() {
+                    Intent intent = new Intent(wocheActivity.this, MainActivity.class);
+                    intent.putExtra("Year",resetYear);
+                    intent.putExtra("Month", resetMonth);
+                    startActivity(intent);
+                    finish();
+                    overridePendingTransition( R.anim.swipe_right_2, R.anim.swipe_right_1);
+                }
+            });
+        }
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.Constraint);
+        layout.setOnTouchListener(new OnSwipeTouchListener(wocheActivity.this) {
+            public void onSwipeTop() {
+
+            }
+
+            public void onSwipeRight() {
+
+            }
+
+            public void onSwipeLeft() {
+
+            }
+
+            public void onSwipeBottom() {
+                Intent intent = new Intent(wocheActivity.this, MainActivity.class);
+                intent.putExtra("Year",resetYear);
+                intent.putExtra("Month", resetMonth);
+                startActivity(intent);
+                finish();
+                overridePendingTransition( R.anim.swipe_right_2, R.anim.swipe_right_1);
             }
         });
     }
 
+    public void setWeek(){
+
+    }
 }
